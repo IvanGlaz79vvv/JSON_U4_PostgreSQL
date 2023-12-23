442667ceab18adc3806651188586de1aa5ae3eef
IvanTest-31 <ivangl79@gmail.com>
2023-12-23 22:28:26 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,126 @@
+package org.example;
+
+import com.google.gson.Gson;
+import netscape.javascript.JSObject;
+import org.apache.commons.collections.MapIterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONString;
+import org.json.JSONStringer;
+import org.postgresql.core.Oid;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.postgresql.core.Oid.JSON;
+import static org.postgresql.core.Oid.JSONB_ARRAY;
+
+public class Main {
+    public static void main(String[] args) throws IOException, SQLException {
+       /* String sqlUsers = "create table if not exists \"Users\" (\n" +
+                "id int generated always as identity,\n" +
+                "name varchar(50) not null,\n" +
+                "primary key(id)\n" +
+                ");";
+
+//        String sqlContacts = "create table \"Contacts\"(\n" +
+//                "id int generated always as identity,\n" +
+//                "\"customerId\" int,\n" +
+//                "\"contactName\" varchar(255) not null,\n" +
+//                "phone varchar(50),\n" +
+//                "email varchar(100),\n" +
+//                "primary key (id),\n" +
+//                "constraint \"fk_Users_Contacts\"\n" +
+//                "foreign key(\"customerId\")\n" +
+//                "references \"Users\"(id)\n" +
+//                ");";
+
+        String sqlContacts = "create table if not exists \"Contacts\"(\n" +
+                "id int generated always as identity,\n" +
+                "customerId int ,\n" +
+                "contactName varchar(255) not null,\n" +
+                "phone varchar(20),\n" +
+                "email varchar(100),\n" +
+                "primary key(id),\n" +
+                "constraint fk_Users_Contacts\n" +              //fk_Users_Contacts\n" +       //fk_customer\n" +
+                "foreign key(\"customerid\")\n" +
+                "references \"Users\"(id)\n" +
+                ");";
+
+        String sqlTest = "create table \"tablicatest\"\n(" +
+                "    employe_id int \n" +
+                "    primary key,\n" +
+                "    first_name    text,\n" +
+                "    last_name     text,\n" +
+                "    email         text,\n" +
+                "    phone_number  text,\n" +
+                "    hire_date     text,\n" +
+                "    job_id        text,\n" +
+                "    salary        text,\n" +
+                "    comission_pct text\n" +
+                ");";
+
+        //////////////////////////////////////////
+        *//**Создание таблицы*//*
+        CreateTable.getcreateTable(sqlUsers);
+        CreateTable.getcreateTable(sqlContacts);
+        CreateTable.getcreateTable(sqlTest);
+
+        *//**Внесение данных*//*
+        String insertUsers = "INSERT INTO \"Users\" (name) VALUES(";
+        BufferedReader brUsers = new BufferedReader(new FileReader("Binder1_work (2).csv"));
+        while (brUsers.ready()) {
+            InsertToTable.getInsertToTable(insertUsers, brUsers);
+        }
+        System.out.println("Users end\n");
+        brUsers.close();
+
+        //>>>>>>>>>>>>>>>>>>>>>>>
+        String insertContacts = "INSERT INTO \"Contacts\" (contactName, phone, email) VALUES(";
+        BufferedReader brContacts = new BufferedReader(new FileReader("Binder1_work (3).csv"));
+        while (brContacts.ready()) {
+            InsertToTable.getInsertToTable(insertContacts, brContacts);
+        }
+        System.out.println("Contacts end\n");
+        brContacts.close();
+
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        String insertTest = "INSERT INTO \"tablicatest\" (employe_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, comission_pct) VALUES(";
+        BufferedReader brTest = new BufferedReader(new FileReader("Binder1_work.csv"));
+        while (brTest.ready()) {
+            InsertToTable.getInsertToTable(insertTest, brTest);
+        }
+        System.out.println("tablicatest end\n");
+        brTest.close();*/
+
+//-------------Задания-----------------------------
+
+        //<<<<<<вывод всех имен и Employee ID>>>>>>>
+//        System.out.println();
+//        for (String s : SelectFormTable.get_Id_Name()) {
+//            System.out.println(s);
+//        }
+
+
+        //<<<<<<вывод фамилии и даты рождения>>>>>>>
+//        System.out.println();
+//        for (String s : SelectFormTable.get_LastName_HireDate()) {
+//            System.out.println(s);
+//        }
+//
+//        //<<<<<<вывод имени и фамилии и Employee ID в порядке убывания номера Employee ID>>>>>>>
+//        System.out.println();
+//        for (String s : SelectFormTable.get_Id_FirstName_LastName_HireDate_DescendingOrder()) {
+//            System.out.println(s);
+//        }
+
+        //<<<<<<вывод из Json>>>>>>>
+        for (JSONArray s : SelectFormTable.get_All())
+            System.out.println(s.get(0) + " " + s.get(1) + " " + s.get(7));
+    }
+}
---
+++ src/main/java/org/example/SelectFormTable.java
@@ -0,0 +1,240 @@
+package org.example;
+
+import org.json.JSONArray;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+public class SelectFormTable {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\033[0;32m";
+    public static final String ANSI_CYAN = ("\033[0;36m");
+    public static final String ANSI_BLUE = "\033[0;34m";
+     List<JSONArray> listJSONArray;
+
+    @Override
+    public String toString() {
+        return "employe_id: " + listJSONArray.get(0) +"\n"+
+                "first_name: " + listJSONArray.get(1) +"\n"+
+                "last_name: " + listJSONArray.get(2) +"\n"+
+                "email: " + listJSONArray.get(3) +"\n"+
+                "phone_number: " + listJSONArray.get(4) +"\n"+
+                "hire_date: " + listJSONArray.get(5) +"\n"+
+                "job_id: " + listJSONArray.get(6) +"\n"+
+                "salary: " + listJSONArray.get(7) +"\n"+
+                "comission_pct: " + listJSONArray.get(8) +"\n";
+    }
+
+    public static List<JSONArray> get_All() throws SQLException {
+
+        List<JSONArray> JSONArray = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT * from tablicatest;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                JSONArray jsonArray = new JSONArray();
+                jsonArray.put(rs.getString("employe_id"));
+                jsonArray.put(rs.getString("first_name"));
+                jsonArray.put(rs.getString("last_name"));
+                jsonArray.put(rs.getString("email"));
+                jsonArray.put(rs.getString("phone_number"));
+                jsonArray.put(rs.getString("hire_date"));
+                jsonArray.put(rs.getString("job_id"));
+                jsonArray.put(rs.getString("salary"));
+                jsonArray.put(rs.getString("comission_pct"));
+                JSONArray.add(jsonArray);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("<<<<<<вывод из Json>>>>>>>");
+        return JSONArray;
+    }
+
+    public static List<String> get_C_and_B() {
+        List<String> listResult = new ArrayList<>();
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT first_name, last_name from tablicatest where first_name like '%b%' and first_name like '%c%';");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("first_name") + " " + rs.getString("last_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("<<<<<<в именах которых есть «b» и «c»>>>>>>>");
+        return listResult;
+    }
+
+    public static List<String> get_First_name_Last_name_Hire_date() {
+
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT first_name, last_name, hire_date from tablicatest \n" +
+                    "where hire_date LIKE '%1987%';");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("hire_date"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("<<<<<<приняты на работу в 1987 году>>>>>>>");
+        return listResult;
+    }///////////////////////////////////////////////////////////////
+
+    public static List<String> get_Max_Salay() {
+        List<String> listResult = new ArrayList<>();
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT first_name,last_name, salary FROM tablicatest WHERE salary = ( SELECT MAX(salary) FROM tablicatest);");
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+            while (rs.next()) {
+                listResult.add(rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("salary"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("<<<<<<максимальная зарплата>>>>>>>");
+        return listResult;
+    }//////////////////////////////////////////////////////////
+
+    public static List<String> get_Min_Salay() {
+        List<String> listResult = new ArrayList<>();
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT first_name,last_name, salary FROM tablicatest WHERE salary = ( SELECT MIN(salary) FROM tablicatest);");//("SELECT first_name,last_name, MIN(salary) FROM tablicatest;");
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+            while (rs.next()) {
+                listResult.add(rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("salary"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("<<<<<<имнимальная зарплата>>>>>>>");
+        return listResult;
+    }/////////////////////////////////////////////////////////////////
+
+    public static List<String> get_even() {
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM tablicatest WHERE employe_id % 2 = 0;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("employe_id") + " " + rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("email") + " " + rs.getString("phone_number") + " " + rs.getString("hire_date") + " " + rs.getString("job_id") + " " + rs.getString("salary") + " " + rs.getString("comission_pct"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("<<<<<<<<<<<<четные записи>>>>>>>>>>>>>>");
+        return listResult;
+    }
+
+    public static List<String> get_odd() {
+
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM tablicatest WHERE employe_id % 2 > 0;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("employe_id") + " " + rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("email") + " " + rs.getString("phone_number") + " " + rs.getString("hire_date") + " " + rs.getString("job_id") + " " + rs.getString("salary") + " " + rs.getString("comission_pct"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("<<<<<<нечетные записи>>>>>>>");
+        return listResult;
+    }
+
+
+
+
+    public static List<String> get_Id_Name() {
+
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT employe_id, first_name from tablicatest;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("employe_id") + " " + rs.getString("first_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("<<<<<<вывод всех имен и Employee ID>>>>>>>");
+        return listResult;
+    }
+
+    public static List<String> get_LastName_HireDate() {
+
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT last_name, hire_date from tablicatest;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("last_name") + " " + rs.getString("hire_date"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("\n<<<<<<вывод фамилии и даты рождения>>>>>>>");
+        return listResult;
+    }
+
+    public static List<String> get_Id_FirstName_LastName_HireDate_DescendingOrder() {
+
+        List<String> listResult = new ArrayList<>();
+
+        try (Connection connect = CreateConnection.getConnect();
+             Statement stmt = connect.createStatement()) {
+            ResultSet rs = stmt.executeQuery("SELECT employe_id, first_name, last_name from tablicatest ORDER BY employe_id;;");
+
+            System.out.println(ANSI_BLUE + "Запрос обработан" + ANSI_RESET);
+
+            while (rs.next()) {
+                listResult.add(rs.getString("employe_id") + " " + rs.getString("first_name") + " " + rs.getString("last_name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("<<<<<<вывод имени и фамилии и Employee ID в порядке убывания " +
+                "номера Employee ID>>>>>>>");
+        return listResult;
+    }
+}
